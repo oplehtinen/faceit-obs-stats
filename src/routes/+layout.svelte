f39f8e0b5321f2dbd6c0d48ad7d0245f437f804d
Olli-Pekka Lehtinen <7263059+oplehtinen@users.noreply.github.com>
2025-09-02 17:15:11 +0000

    Refactor layout component to improve responsiveness; adjust height classes based on home page status

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,21 @@
 	import { onMount } from 'svelte';
 	import { tick } from 'svelte';
 	import '$lib/poller';
+	import { page } from '$app/stores';
 	export let data;
 	onMount(async () => {
 		await tick();
 		console.log(data);
 	});
+	$: isHome = $page.url.pathname === '/';
 </script>
 
 <div
-	class="flex py-2 items-center justify-center h-screen animate-text bg-gradient-to-b from-secondary from-10% via-accent via-30% to-base-content to-90%"
+	class={`flex py-2 items-center justify-center ${isHome ? 'min-h-screen' : 'h-screen'} animate-text bg-gradient-to-b from-secondary via-accent to-base-content`}
 >
-	<div class="container-fluid mx-auto my-4 px-8 gap-4 flex flex-col h-full w-full justify-evenly">
+	<div
+		class={`container-fluid mx-auto my-4 px-8 gap-4 flex flex-col w-full justify-evenly ${isHome ? '' : 'h-full'}`}
+	>
 		<slot />
 	</div>
 </div>
